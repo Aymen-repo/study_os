7047b243a1bbe6f57f7b0ef7da5ff2b4395ac1bf
aymen <aymensstm@gmail.com>
2026-04-07 10:22:52 +0100

    i register the models in the admin pannel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Subject, Task
+
+admin.site.register(Subject)
+admin.site.register(Task)
